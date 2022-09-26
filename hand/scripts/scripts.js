e084dace1a2667275e37116ac6236dfad45f5cc3
vi0lia <oliviaelanza@gmail.com>
2022-09-26 11:46:40 -0700

    copied from original

---
+++ hand/scripts/scripts.js
@@ -0,0 +1,9 @@
+
+function gay() {
+  var x = document.getElementById("myDIV","myDIV2","myDIV3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
